32b4d057a87a80d12ef968545e08df77c7fbd203
Maxime Boulin <boulinm@amazon.com>
2020-07-27 16:14:47 -0700

    cleanup

--- minimal-player-setup/script.js
+++ minimal-player-setup/script.js
@@ -16,9 +16,13 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+// Playback configuration
+// Replace this with your own Amazon IVS Playback URL
+const playbackUrl = 'https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8'
+
 if (IVSPlayer.isPlayerSupported) {
   const player = IVSPlayer.create();
   player.attachHTMLVideoElement(document.getElementById('video-player'));
-  player.load('https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8');
+  player.load(playbackUrl);
   player.play();
 }
